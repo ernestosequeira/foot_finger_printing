0344623af30f4ea63a44b6bbd4865b78043ed42f
Ernesto Sequeira <erdaseq@gmail.com>
2013-09-19 14:05:32 -0300

    specific port scan

--- unlp_security.sh
+++ unlp_security.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ########### Autor: Ernesto Sequeira ###############################################
-########### UNLP - Seguridad y Privacidad en Redes de Datos - D - 2013 ############
+########### Seguridad y Privacidad en Redes de Datos 2013 ############
 
 # Colors constants
 NONE="$(tput sgr0)"
@@ -25,7 +25,7 @@ message() {
 }
 
 install_packages() {
-	sudo aptitude install nmap -y > /dev/null
+	sudo aptitude install nmap hping3 -y > /dev/null 
 }
 
 main_menu() {
@@ -34,15 +34,15 @@ main_menu() {
 	do
 		echo " Main Menu: "
 		echo " -----------"
-		echo "[1]. Footprinting (non-intrusive)"
-		echo "[2]. Fingerprinting(intrusive)"
-		echo "[3]. Other"
+		echo "[1]. Footprinting (non-intrusive)."
+		echo "[2]. Fingerprinting (intrusive)."
+		echo "[3]. Specific port scan (intrusive)."
 		echo "[4]. Exit"
 		read -p "Select an option [1-4]: " option
 	case $option in
 		1) footprinting;;
 		2) fingerprinting;;
-		3) other;;
+		3) port_scan;;
 		4) exit 0;;
 		*) echo "$option is an invalid option.";
 		echo "Press any key to finish continue...";
@@ -96,12 +96,30 @@ fingerprinting(){
 	message "$(sudo nmap -iflist $ip)" ${RED}
 	message_date ">> Scanning complete." ${GREEN}
 	
-
 	echo "Press any key to finish..."
 	read p
 	clear
 }
 
+port_scan(){
+	echo "Input host or ip: (example: 192.168.1.33, unlp.edu.ar)"
+	read host
+	echo "Input port number: (example: 21)"
+	read port
+	message_date ">> Start scanning port $port." ${GREEN}
+	sudo hping3 -S $host -p $port -c 1 | grep flags > scan_4.out
+	port_scanning=$(awk '{ print $7 }' FS=" " scan_4.out)
+	if [ $port_scanning == "flags=SA" ]; then
+		message ">> Port:$port Open" ${RED}
+	elif [ $port_scanning == "flags=RA" ]; then
+		message ">> Port:$port Close." ${RED}
+	else
+		message ">> Port:$port, Ping host protected ." ${RED}
+	fi
+	message_date ">> Scanning complete." ${GREEN}
+}
+
+# Call funtions
 install_packages
 main_menu
 
